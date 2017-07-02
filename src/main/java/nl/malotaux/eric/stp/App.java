55f860ea32760b7ca8a278d96789dde8c82f5926
Eric Jan Malotaux <emalotau@gmail.com>
2017-07-02 11:31:09 +0200

    Input spreadsheet in het project getrokken.

--- src/main/java/nl/malotaux/eric/stp/App.java
+++ src/main/java/nl/malotaux/eric/stp/App.java
@@ -3,13 +3,16 @@ package nl.malotaux.eric.stp;
 import io.vavr.collection.List;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroupFile;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.DateFormat;
@@ -19,14 +22,14 @@ import java.util.Date;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InvalidFormatException {
         ST template = new STGroupFile("nl/malotaux/eric/stp/service.stg").getInstanceOf("services");
-        template.add("services", readServices("/Users/ema21214/Downloads/Voorbeeld.xlsx"));
+        template.add("services", readServices("nl/malotaux/eric/stp/Voorbeeld.xlsx"));
         Files.write(Paths.get("target/services.html"), template.render().getBytes());
     }
 
-    private static java.util.List<Service> readServices(String pad) throws IOException {
-        try (XSSFWorkbook workbook = new XSSFWorkbook((new FileInputStream(pad)))) {
+    private static java.util.List<Service> readServices(String pad) throws IOException, InvalidFormatException {
+        try (XSSFWorkbook workbook = new XSSFWorkbook(ClassLoader.getSystemResource(pad).openStream())) {
             XSSFSheet sheet = workbook.getSheetAt(0);
             return List.range(12, 27).map(sheet::getRow).map(row -> new Service(
                     row.getCell(0).getDateCellValue(),
